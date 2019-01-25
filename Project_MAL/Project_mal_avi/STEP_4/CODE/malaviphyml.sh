d6b2c6766fdf2eb74e0cc0b16494dd7949ec7ef6
RaphaelCA <46780835+RaphaelCA@users.noreply.github.com>
2019-01-25 14:06:25 +0100

    Update malaviphyml.sh

--- Project_MAL/Project_mal_avi/STEP_4/CODE/malaviphyml.sh
+++ Project_MAL/Project_mal_avi/STEP_4/CODE/malaviphyml.sh
@@ -7,12 +7,6 @@ clustalw -INFILE=NODUPSEQSPP_MAL.fas -TYPE=DNA -OUTFILE=out_ali.phy -OUTPUT=PHYL
 phyml -i out_ali.phy -d nt -n 1 -m HKY85
 nw_display -s -S -v 25 -b 'opacity:0' -i 'font-size:8' out_ali.phy_phyml_tree.txt > all_seq_ali.phy_phyml_tree.svg
 
-#move all output to RESULTS and remove original data from CODE
-#mv all_seq_ali.phy_phyml_tree.svg ~/Documents/Project_MAL/Project_mal_avi/STEP_4/RESULTS
-#mv NODUPSEQSPP_MAL.dnd ~/Documents/Project_MAL/Project_mal_avi/STEP_4/RESULTS
-#mv out_ali.phy ~/Documents/Project_MAL/Project_mal_avi/STEP_4/RESULTS
-#mv out_ali.my_phyml_stats.txt ~/Documents/Project_MAL/Project_mal_avi/STEP_4/RESULTS
-#mv out_ali.phy_phyml_tree.txt ~/Documents/Project_MAL/Project_mal_avi/STEP_4/RESULTS
-rm NODUPSEQSPP_MAL.fas
+
 
 
